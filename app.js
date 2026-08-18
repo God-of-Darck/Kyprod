cd2bc418dd4ca60c429a312ddbbddcda57e1b253
João Daniel Labres Lima <159195521+God-of-Darck@users.noreply.github.com>
2026-08-18 11:49:23 -0300

    Update API token in app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const API_URL = "https://api.baserow.io/api/database/rows/table/763804/?user_field_names=true";
-const API_TOKEN = "9if2RRgZA3yBNJ2OExtNtszWOfcb9yeE";
+//const API_TOKEN = "9if2RRgZA3yBNJ2OExtNtszWOfcb9yeE";
+const API_TOKEN = "ygro5VqzDxW6FMIf1WVYw10se06qhjum";
 
 let dadosCompletos = [];
 
